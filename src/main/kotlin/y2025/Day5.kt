d3ec5fd1165c088713178d4fae71697f48a7c2b2
Ayoub Khobalatte <ayoub.khobalatte@penneo.com>
2025-12-05 11:47:41 +0100

    feat: day 5 completed

--- src/main/kotlin/y2025/Day5.kt
+++ src/main/kotlin/y2025/Day5.kt
@@ -3,7 +3,89 @@
 package y2025
 
 class Day5 : Day() {
-    override fun solvePart1(file: String): Long = 0L
+    override fun solvePart1(file: String): Long {
+        val lines = readLines(file)
 
-    override fun solvePart2(file: String): Long = 0L
+        val ids = mutableListOf<Long>()
+        val ranges = mutableListOf<LongRange>()
+
+        lines.forEach {
+            if (it.contains("-")) {
+                val split = it.split("-")
+                ranges.add(split[0].toLong()..split[1].toLong())
+                return@forEach
+            }
+
+            ids.add(it.toLong())
+        }
+
+        var result = 0L
+        for (id in ids) {
+            for (range in ranges) {
+                if (range.contains(id)) {
+                    result++
+                    break
+                }
+            }
+        }
+
+        return result
+    }
+
+    override fun solvePart2(file: String): Long {
+        val ranges = readLines(file)
+            .filter { it.contains("-") }
+            .map {
+                val split = it.split("-")
+                split[0].toLong()..split[1].toLong()
+            }
+
+        var min = Long.MAX_VALUE
+        var max = 0L
+        ranges.forEach { range ->
+            min = min.coerceAtMost(range.first)
+            max = max.coerceAtLeast(range.last)
+        }
+
+        val bigassRange = min..max
+        var negatives = listOf(min..max)
+
+        ranges.forEach {
+            negatives = minus(negatives, it)
+        }
+
+        var positives = listOf(min..max)
+        negatives.forEach {
+            positives = minus(positives, it)
+        }
+
+        return positives.sumOf { it.last - it.first + 1 }
+    }
+
+    private fun minus(
+        ranges: List<LongRange>,
+        rangeB: LongRange,
+    ): List<LongRange> = ranges.flatMap { minus(it, rangeB) }
+
+    private fun minus(
+        range: LongRange,
+        other: LongRange,
+    ): List<LongRange> {
+        val result = mutableListOf<LongRange>()
+        if (range.last < other.first || other.last < range.first) {
+            return listOf(range)
+        }
+        if (other.first > range.first) {
+            result += range.first until other.first
+        }
+
+        if (other.last < range.last) {
+            result += (other.last + 1)..range.last
+        }
+
+        return result
+    }
 }
+
+fun <T : Comparable<T>> ClosedRange<T>.includes(other: ClosedRange<T>): Boolean =
+    this.start <= other.start && this.endInclusive >= other.endInclusive
--- src/test/kotlin/y2025/Day5Test.kt
+++ src/test/kotlin/y2025/Day5Test.kt
@@ -6,27 +6,26 @@ import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-@Ignore
 class Day5Test {
     private val day = Day5()
 
     @Test
     fun testExample1() {
-        assertEquals(-1L, day.solvePart1("src/main/resources/2025/day5/example.txt"))
+        assertEquals(3L, day.solvePart1("src/main/resources/2025/day5/example.txt"))
     }
 
     @Test
     fun testInput1() {
-        assertEquals(-1L, day.solvePart1("src/main/resources/2025/day5/input.txt"))
+        assertEquals(756L, day.solvePart1("src/main/resources/2025/day5/input.txt"))
     }
 
     @Test
     fun testExample2() {
-        assertEquals(-1L, day.solvePart2("src/main/resources/2025/day5/example.txt"))
+        assertEquals(14L, day.solvePart2("src/main/resources/2025/day5/example.txt"))
     }
 
     @Test
     fun testInput2() {
-        assertEquals(-1L, day.solvePart2("src/main/resources/2025/day5/input.txt"))
+        assertEquals(355555479253787L, day.solvePart2("src/main/resources/2025/day5/input.txt"))
     }
 }
