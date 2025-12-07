6545ed40f09e9f0dd83f3ea78196384d2c26ca18
Ayoub Khobalatte <ayoub.khobalatte@penneo.com>
2025-12-07 13:42:30 +0100

    feat: prepare for day 8

---
+++ src/main/kotlin/y2025/Day8.kt
@@ -0,0 +1,9 @@
+@file:Suppress("SpellCheckingInspection")
+
+package y2025
+
+class Day8 : Day() {
+    override fun solvePart1(file: String): Long = 0L
+
+    override fun solvePart2(file: String): Long = 0L
+}
